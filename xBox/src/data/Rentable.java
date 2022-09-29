e3d255341ffb6a3b6f8f6e6d6e7ef131b48a37b9
yzhou442 <yzhou020404@gmail.com>
2022-09-30 00:11:55 +0800

    Add several data files.

--- xBox/src/data/Rentable.java
+++ xBox/src/data/Rentable.java
@@ -1,14 +1,71 @@
-/**
- * 
- */
 package data;
 
-/**
- * @author xyli45
- * 
- * Description:
- * This is an interface for stuffs to implement. (i.e., boxes, luggage)
- */
-public interface Rentable {
 
+public abstract class Rentable implements Comparable<Rentable>{
+    private String id,name;
+    private RentableStatus status;
+
+
+    public Rentable(String i, String n){
+        id=i;
+        name=n;
+        status=new RentableStatusAvailable();
+    }
+
+    public void lendOut(Client c)
+    {
+
+        Day d=SystemDate.getInstance().clone();
+        status=new RentableStatusOccupied(d, c);
+        RentableManager rm=RentableManager.getInstance();
+        rm.lendOutRentable(this);
+    }
+
+    public void getBack(Client c)
+    {
+        status=new RentableStatusAvailable();
+        RentableManager rm=RentableManager.getInstance();
+        rm.getBackRentable(this);
+        
+    }
+
+
+    @Override
+    public int compareTo(Rentable o) {
+        return this.id.compareTo(o.id);
+    }
+
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public abstract String getType();
+    
+
+
+    @Override
+    public String toString() {
+        return String.format("%-5s%-20s%-9s", id, name, status.toString());
+    }
+
+    public String getIdAndName() {
+        return id+" "+name;
+    }
+
+    public RentableStatus getStatus(){
+        return status;
+    }
+
+    public void setAvailale(){
+        status=new RentableStatusAvailable();
+    }
+
+    public void setOccupied(Day d, Client c){
+        status=new RentableStatusOccupied(d,c);
+    }
 }
