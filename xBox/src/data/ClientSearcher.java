1ad96030370205ef195786da2bc2f328e7813059
yzhou442 <yzhou020404@gmail.com>
2022-10-05 15:50:38 +0800

    update: ClientSearcher.java

--- xBox/src/data/ClientSearcher.java
+++ xBox/src/data/ClientSearcher.java
@@ -10,14 +10,14 @@ public class ClientSearcher {
 		return searcher;
 	}
 	
-	public ArrayList<Client> searchByClientEmail(String aEmail)
+	public Client searchByClientEmail(String aEmail)
 	{
 		ClientStorer storer=ClientStorer.getInstance();
-		ArrayList<Client> Clientlist=storer.getList(),result=new ArrayList<>();
+		ArrayList<Client> Clientlist=storer.getList();
 		for(Client r:Clientlist)
 			if(r.getEmail().equals(aEmail))
-				result.add(r);
-		return result;
+				return r;
+		return null;
 	}
 	
 }
