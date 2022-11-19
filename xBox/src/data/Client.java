20f56f728d21f98b27797c64a152737665f7df11
yzhou442 <yzhou020404@gmail.com>
2022-11-20 02:25:19 +0800

    modified Client by deleting borrowCount. Added test for Client, Rentable, RentableStatus.

--- xBox/src/data/Client.java
+++ xBox/src/data/Client.java
@@ -25,6 +25,7 @@ public abstract class Client{
     
     public String getPassword() { return password; } // TODO: 这是哪个小可爱写的
     
+    
     @Override
     public String toString() { return String.format("%-25s%-10s", this.email, this.phoneNo); }
     
