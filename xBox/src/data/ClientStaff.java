e3273add4e04d63122d242c212e27a4a7fd0f906
yzhou442 <yzhou020404@gmail.com>
2022-11-20 02:25:19 +0800

    modified Client by deleting borrowCount. Added test for Client, Rentable, RentableStatus.

--- xBox/src/data/ClientStaff.java
+++ xBox/src/data/ClientStaff.java
@@ -2,17 +2,17 @@ package data;
 
 public class ClientStaff extends Client{
     private int borrowedCount;
-
+    
     public ClientStaff(String email, String phoneNo, String password) {
 		super(email, phoneNo, password);
 		this.borrowedCount = getMaxBorrowedCount(); 
 	}
     
-    @Override
     public void changeBorrowedCount(int number) {
         this.borrowedCount += number;
     }
-    
+
+
     @Override
     public int getBorrowCount() { return this.borrowedCount; }
 
